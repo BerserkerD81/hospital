a15215e00d86cc418a0d57aaaf90f9cde8214963
BerserkerD81 <jmigueles20@alumnos.utalca.cl>
2023-10-28 17:03:10 -0300

    javafx-chat-seleccionar-aquien hablar y eleminar usuarios desconectados

--- src/main/java/com/hospital/hospital/HelloApplication.java
+++ src/main/java/com/hospital/hospital/HelloApplication.java
@@ -1,59 +1,131 @@
 package com.hospital.hospital;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
-import javafx.stage.Stage;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class HelloApplication extends Application {
     private VBox chatPlace;
+    private Socket socket;
+    private PrintWriter out;
+
+    public String UsuariosActivos ="";
 
     @Override
     public void start(Stage stage) throws IOException {
+        // Pedir al usuario que ingrese su nombre por consola
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Por favor, ingrese su nombre de usuario: ");
+        String userName = scanner.nextLine();
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
         stage.setTitle("Chat Hospital");
         stage.setScene(scene);
         stage.show();
 
-        // Obtener el VBox chat_place desde el FXML
         chatPlace = (VBox) scene.lookup("#chat_place");
 
-        // Manejar el evento de teclado para la tecla "Esc"
-        scene.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.ESCAPE) {
-                if (chatPlace != null) {
-                    // Crear un nuevo chat_element similar al existente
+        // Conectarse al servidor
+        socket = new Socket("localhost", 9090); // Reemplazar "localhost" con la IP real si es necesario
 
-                    HBox DUPLICADO =createDuplicateStructure();
-                    chatPlace.getChildren().add(DUPLICADO);
+        // Enviar el nombre de usuario al servidor
+        out = new PrintWriter(socket.getOutputStream(), true);
+        out.println(userName);
+
+        // Setting up a thread to continuously listen for server messages
+        Thread listenThread = new Thread(() -> {
+            try {
+                Scanner in = new Scanner(socket.getInputStream());
+                while (true) {
+                    if (in.hasNextLine()) {
+                        String message = in.nextLine();
+
+                            Platform.runLater(() -> {
+                                if(!(message.split(":")[0].contains("Desconectados")))
+                                {
+                                    String[] usuarios= message.split(", ");
+
+                                    for (String part : usuarios) { // Recorrer cada elemento del mensajeff
+
+
+                                        if(!userName.equals(part) && !UsuariosActivos.contains(part))
+                                        {
+
+                                            UsuariosActivos=UsuariosActivos+part+",";
+                                            HBox elementBox = createDuplicateStructure(part);
+                                            chatPlace.getChildren().add(elementBox);
+                                        }
+
+                                    }
+                                }
+
+                                if(message.split(":")[0].contains("Desconectados")){
+                                    System.out.println(message);
+                                    String userToDisconnect = message.split(": ")[1];
+                                    System.out.println(userToDisconnect);
+                                    Iterator<Node> iterator = chatPlace.getChildren().iterator();
+                                    while (iterator.hasNext()) {
+                                        Node node = iterator.next();
+                                        if (node instanceof HBox hbox) {
+                                            for (Node childNode : hbox.getChildren()) {
+                                                if (childNode instanceof VBox vbox) {
+                                                    for (Node vboxChild : vbox.getChildren()) {
+                                                        if (vboxChild instanceof Label label && label.getText().equals(userToDisconnect)) {
+                                                            System.out.println("Se encontró el texto en la etiqueta: " + label.getText());
+                                                            iterator.remove(); // Utiliza el iterador para eliminar el elemento de manera segura
+                                                            break;
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                }
+
+
+
+                            });
+
+
+
+                    }
                 }
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         });
+        listenThread.start();
     }
-
-
-
-
-
-    public HBox createDuplicateStructure() {
+    public HBox createDuplicateStructure(String user) {
         HBox hBox = new HBox();
 
-        ImageView imageView = new ImageView(new Image("https://cdn-icons-png.flaticon.com/512/64/64572.png"));
+        ImageView imageView;
+        imageView = new ImageView(new Image("https://cdn-icons-png.flaticon.com/512/64/64572.png"));
         imageView.setFitHeight(60);
         imageView.setFitWidth(69);
         imageView.setPreserveRatio(true);
@@ -63,7 +135,7 @@ public class HelloApplication extends Application {
         vBox.setPrefWidth(185);
         vBox.setAlignment(javafx.geometry.Pos.CENTER);
 
-        Label label = new Label("Name_placeholder");
+        Label label = new Label(user);
         label.setPrefHeight(18);
         label.setPrefWidth(161);
         label.setTextFill(Color.WHITE);
@@ -81,6 +153,22 @@ public class HelloApplication extends Application {
 
         // Adding a 10-pixel top margin to the HBox
         HBox.setMargin(hBox, new Insets(10, 0, 0, 0));
+
+        // Agregar un controlador de eventos al HBox para cambiar el fondo al hacer clic
+        hBox.setOnMouseClicked(event -> {
+            // Recorrer todos los hijos del chatPlace para quitar el fondo coloreado
+            for (Node node : chatPlace.getChildren()) {
+                if (node instanceof HBox) {
+                    node.setStyle(""); // Establecer el estilo vacío para eliminar el fondo coloreado
+                }
+            }
+            hBox.setStyle("-fx-background-color: purple;"); // Cambiar el fondo a morado al hacer clic en este HBox
+
+            String username = label.getText();
+            // Imprimir en la consola el mensaje "Hablando con [nombre del usuario]"
+            System.out.println("Hablando con " + username);
+
+        });
 
         return hBox;
     }
